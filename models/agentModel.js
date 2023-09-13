61cbd655a2891fba8ed543381508ecb60b5d1e28
Mardochee Luviki <m.luviki.ext@funecap.com>
2023-09-13 10:05:57 +0200

    fix agents model

--- models/agentModel.js
+++ models/agentModel.js
@@ -20,80 +20,80 @@ const agentSchema = new mongoose.Schema({
         type: String,
         required: [true, "Renseignez le prenom"],
       },
-    lieuNaissance: {
-        type: String,
-      },
-    dateNaissance: {
-        type: Date,
-      },
-    nationalite: {
-        type: String,
-      },
-    genre: {
-        type: String,
-        required: [true, "Renseignez le genre"],
-      },  
-    provinceaffectation: {
-        type: String,
-      },
-    territoireaffectation: {
-        type: String,
-      },
-    etatcivile: {
-        type: String,
-        required: [true, "renseignez votre état civile"],
-      },
+    // lieuNaissance: {
+    //     type: String,
+    //   },
+    // dateNaissance: {
+    //     type: Date,
+    //   },
+    // nationalite: {
+    //     type: String,
+    //   },
+    // genre: {
+    //     type: String,
+    //     required: [true, "Renseignez le genre"],
+    //   },  
+    // provinceaffectation: {
+    //     type: String,
+    //   },
+    // territoireaffectation: {
+    //     type: String,
+    //   },
+    // etatcivile: {
+    //     type: String,
+    //     required: [true, "renseignez votre état civile"],
+    //   },
     adressehome: {
         type: String,
         required: [true, "Renseignez l adresse"],
       },
-    tel: {
-        type: Number,
-        required: [true, "Renseignez le numéro de téléphone"],
-      },
-    email: {
-        type: String,
-        validate: [validator.isEmail, "Vérifiez que vous avez bien saisi l'adresse mail svp !'"]
-      },
+    // tel: {
+    //     type: Number,
+    //     required: [true, "Renseignez le numéro de téléphone"],
+    //   },
+    // email: {
+    //     type: String,
+    //     validate: [validator.isEmail, "Vérifiez que vous avez bien saisi l'adresse mail svp !'"]
+    //   },
     matricule: {
         type: String,
         required: [true, "Renseignez le numéro matricule"],
       },
-    direction: {
-        type: String,
-      },
-    division: {
-        type: String,
-      },
-    bureau: {
-        type: String,
-      },
+    // direction: {
+    //     type: String,
+    //   },
+    // division: {
+    //     type: String,
+    //   },
+    // bureau: {
+    //     type: String,
+    //   },
       grade: {
         type: String,
     },
     fonction: {
       type: String,
   },
-    inss: {
-        type: String,
-        required: [true, "Renseignez le numéro inss"],
-      },
+    // inss: {
+    //     type: String,
+    //     required: [true, "Renseignez le numéro inss"],
+    //   },
 
-    diplome: {
-        type: String,
-        required: [true, "Renseignez le numéro inss"],
-      },
-    salaire: {
-        type: Number,
-        required: [true, "Renseignez votre salaire"],
-      },
-    nombreenfants: {
-        type: Number,
-      },
-    datengagement: {
-        type: Date,
-        required: [true, "Renseignez la date d affectation"],
-      },
+    // diplome: {
+    //     type: String,
+    //     required: [true, "Renseignez le numéro inss"],
+    //   },
+    // salaire: {
+    //     type: Number,
+    //     required: [true, "Renseignez votre salaire"],
+    //   },
+    // nombreenfants: {
+    //     type: Number,
+    //   },
+    // datengagement: {
+    //     type: Date,
+    //     required: [true, "Renseignez la date d affectation"],
+    //   },
     photo: {
         type: String,
         required: [true, "Renseignez la photo de l agent"],
