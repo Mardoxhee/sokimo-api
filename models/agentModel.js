c3d793ec4889c16fc04fa107f91b84e8a1613356
Mardochee Luviki <m.luviki.ext@funecap.com>
2023-09-20 10:51:25 +0200

    add active agent input

--- models/agentModel.js
+++ models/agentModel.js
@@ -20,29 +20,29 @@ const agentSchema = new mongoose.Schema({
         type: String,
         required: [true, "Renseignez le prenom"],
       },
-    // lieuNaissance: {
-    //     type: String,
-    //   },
-    // dateNaissance: {
-    //     type: Date,
-    //   },
-    // nationalite: {
-    //     type: String,
-    //   },
-    // genre: {
-    //     type: String,
-    //     required: [true, "Renseignez le genre"],
-    //   },  
-    // provinceaffectation: {
-    //     type: String,
-    //   },
-    // territoireaffectation: {
-    //     type: String,
-    //   },
-    // etatcivile: {
-    //     type: String,
-    //     required: [true, "renseignez votre état civile"],
-    //   },
+    lieuNaissance: {
+        type: String,
+      },
+    dateNaissance: {
+        type: Date,
+      },
+    nationalite: {
+        type: String,
+      },
+    genre: {
+        type: String,
+        required: [true, "Renseignez le genre"],
+      },  
+    provinceaffectation: {
+        type: String,
+      },
+    territoireaffectation: {
+        type: String,
+      },
+    etatcivile: {
+        type: String,
+        required: [true, "renseignez votre état civile"],
+      },
     adressNumero: {
         type: String,
         required: [true, "Resignez un numéro de domicile"],
@@ -59,64 +59,69 @@ const agentSchema = new mongoose.Schema({
       type: String,
       required: [true, "Renseignez lavenue"],
     },
-    adressehome: {
+    
+    adressehome : {
       type: String,
       // required: [true, "Renseignez lavenue"],
     },
-    // tel: {
-    //     type: Number,
-    //     required: [true, "Renseignez le numéro de téléphone"],
-    //   },
-    // email: {
-    //     type: String,
-    //     validate: [validator.isEmail, "Vérifiez que vous avez bien saisi l'adresse mail svp !'"]
-    //   },
+
+    tel: {
+        type: Number,
+        // required: [true, "Renseignez le numéro de téléphone"],
+      },
+    email: {
+        type: String,
+        validate: [validator.isEmail, "Vérifiez que vous avez bien saisi l'adresse mail svp !'"]
+      },
     matricule: {
         type: Number,
         required: [true, "Renseignez le numéro matricule"],
         unique: [true, "deux agents ne peuvent pas avoir le même matricule"],
       },
-    // direction: {
-    //     type: String,
-    //   },
-    // division: {
-    //     type: String,
-    //   },
-    // bureau: {
-    //     type: String,
-    //   },
+    direction: {
+        type: String,
+      },
+    division: {
+        type: String,
+      },
+    bureau: {
+        type: String,
+      },
       grade: {
         type: String,
     },
     fonction: {
       type: String,
   },
-    // inss: {
-    //     type: String,
-    //     required: [true, "Renseignez le numéro inss"],
-    //   },
+    inss: {
+        type: String,
+        required: [true, "Renseignez le numéro inss"],
+      },
 
-    // diplome: {
-    //     type: String,
-    //     required: [true, "Renseignez le numéro inss"],
-    //   },
-    // salaire: {
-    //     type: Number,
-    //     required: [true, "Renseignez votre salaire"],
-    //   },
-    // nombreenfants: {
-    //     type: Number,
-    //   },
-    // datengagement: {
-    //     type: Date,
-    //     required: [true, "Renseignez la date d affectation"],
-    //   },
+    diplome: {
+        type: String,
+        required: [true, "Renseignez le numéro inss"],
+      },
+    salaire: {
+        type: Number,
+        required: [true, "Renseignez votre salaire"],
+      },
+    nombreenfants: {
+        type: Number,
+      },
+    datengagement: {
+        type: Date,
+        required: [true, "Renseignez la date d affectation"],
+      },
     entite: {
       type: String,
   },
+  actif: {
+    type: Boolean,
+},
     photo: {
         type: String,
-        required: [true, "Renseignez la photo de l agent"],
+        // required: [true, "Renseignez la photo de l agent"],
       },
 })
 
